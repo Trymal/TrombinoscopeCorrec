0f656a2d6c7d59d746c9978abca0b2f306590981
BuildTools <unconfigured@null.spigotmc.org>
2020-04-26 13:52:25 +0200

    api groupe

--- siteEtu/api.php
+++ siteEtu/api.php
@@ -1,6 +1,7 @@
 <?php
 
 	$etus = array();
+	
 	if ($_GET['option'] == 'classe') {
 		$classe = $_GET['classe'];
 		$fichier = file('./comptes.csv');
@@ -12,14 +13,24 @@
 				array_push($etus, $infos);
 			}
 		}
-		$json = json_encode($etus);
-		header("Content-type: application/json");
-		echo $json;
-
 	}
 
 	elseif ($_GET['option'] == 'groupe') {
-		
+		$classe = $_GET['classe'];
+		$groupe = $_GET['groupe'];
+		$fichier = file('./comptes.csv');
+		for ($i=0; $i < sizeof($fichier); $i++) { 
+			$infos = explode(';', $fichier[$i]);
+			$infos = array_slice($infos, 0, sizeof($infos)-2);
+			unset($infos[3]);
+			if ($infos[5] == $classe && $infos[6] == $groupe) {
+				array_push($etus, $infos);
+			}
+		}
 	}
+
+	$json = json_encode($etus);
+		header("Content-type: application/json");
+		echo $json;
 
 ?>
