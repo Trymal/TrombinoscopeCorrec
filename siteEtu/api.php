baf47df91357fd0485d4fcc68c27b8bca226f7a0
BuildTools <unconfigured@null.spigotmc.org>
2020-04-26 13:40:17 +0200

    return infos classe API

--- siteEtu/api.php
+++ siteEtu/api.php
@@ -6,11 +6,16 @@
 		$fichier = file('./comptes.csv');
 		for ($i=0; $i < sizeof($fichier); $i++) { 
 			$infos = explode(';', $fichier[$i]);
+			$infos = array_slice($infos, 0, sizeof($infos)-2);
+			unset($infos[3]);
 			if ($infos[5] == $classe) {
 				array_push($etus, $infos);
 			}
 		}
-		print_r($etus);
+		$json = json_encode($etus);
+		header("Content-type: application/json");
+		echo $json;
+
 	}
 
 	elseif ($_GET['option'] == 'groupe') {
